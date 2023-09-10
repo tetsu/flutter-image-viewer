db3912a735756caa34df993f6b3bfb9aa16940ea
Tetsuro Mor <tetsuromori@gmail.com>
2023-09-11 03:16:54 +0900

    lesson 112

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -14,14 +14,42 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(20.0),
-      child: const Form(
+      child: Form(
         child: Column(
           children: [
-            Text('Text 1'),
-            Text('Text 2'),
+            emailField(),
+            passwordField(),
+            submitButton(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      child: const Text('Submit'),
+      onPressed: () {},
     );
   }
 }
